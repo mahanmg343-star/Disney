8aaf656cbc7612ce9fe6e6e50fbc5648c5dcf463
mahanmg343-star <mahanmg343@gmail.com>
2026-06-28 14:09:45 +0330

    disney

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './commponent/Header/Header'
+
+function App() {
+  return (
+    <>
+    
+    <Header/>
+    
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/commponent/Header/Header.jsx
@@ -0,0 +1,125 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+
+    <section className='container-fluid'>
+    <section className='row'>
+
+      <div className=''>
+
+    <div className='d-block col '>
+
+    <nav className="navbar bg-body-tertiary">
+  <div className="container-fluid">
+
+
+    <div className='row d-flex align-items-center g-2'>
+
+    <a className="navbar-brand d-flex" href="https://en.disneyme.com/">
+      <img src="https://static-mh.content.disney.io/matterhorn/assets/goc/disney_logo_dark@2x-45d70f7dd57b.png" alt="Logo" width="79" height="40" className="d-inline-block align-text-top"/>
+      
+      <div className="dropdown">
+  <p className="btn btn-light border " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    DISNEY+
+  </p>
+
+  <ul className="dropdown-menu">
+    <li><a className="dropdown-item" href="https://www.apps.disneyplus.com/onboarding?utm_source=Referral&utm_medium=Disney_WebsiteMENA&utm_campaign=Backlink&utm_content=Homepage_Navigation">Find out more</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/introducing-disney-plus">Introducing Disney+</a></li>
+   
+  </ul>
+</div>
+<div className="dropdown">
+  <p className="btn btn-light border " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    MOVIES
+  </p>
+
+  <ul className="dropdown-menu">
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies">Featured</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/hoppers">Hoppers</a></li>
+   <li><a className="dropdown-item" href="https://en.disneyme.com/movies/the-devil-wears-prada-2">The Devil Prada2</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/star-wars-the-mandalorian-and-grogu">Star Wars:The Mandalorian and Grogu</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/toy-story-5">Toy Story 5</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/moana-2026">Moana</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/avengers-doomsday">Avengers:Doomday</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/lucasfilm">Lucasfilm Movies</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/marvel">Marvel Movies</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/pixar">Pixar Movies</a></li>
+  </ul>
+</div>
+    </a>
+    </div>
+  </div>
+</nav>
+    </div>
+      </div>
+
+    </section>
+    </section>
+    
+
+
+    <section className='container-fluid'>
+    <section className='row'>
+
+<div className=''>
+
+    <div className='d-none md-d-block'>
+      <img src="https://static-mh.content.disney.io/matterhorn/assets/goc/disney_logo_dark@2x-45d70f7dd57b.png" alt="Logo" width="79" height="40" className="d-inline-block align-text-top"/>
+
+    <button className="btn btn-light" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">=</button>
+<div className="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title" id="offcanvasTopLabel">
+            <img src="https://static-mh.content.disney.io/matterhorn/assets/goc/disney_logo_dark@2x-45d70f7dd57b.png" alt="Logo" width="79" height="40" className="d-inline-block align-text-top"/>
+    </h5>
+    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div className="offcanvas-body">
+     <div className="dropdown">
+  <p className="btn btn-light border " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    DISNEY+
+  </p>
+
+  <ul className="dropdown-menu">
+    <li><a className="dropdown-item" href="https://www.apps.disneyplus.com/onboarding?utm_source=Referral&utm_medium=Disney_WebsiteMENA&utm_campaign=Backlink&utm_content=Homepage_Navigation">Find out more</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/introducing-disney-plus">Introducing Disney+</a></li>
+   
+  </ul>
+</div>
+<div className="dropdown">
+  <p className="btn btn-light border " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    MOVIES
+  </p>
+
+  <ul className="dropdown-menu">
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies">Featured</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/hoppers">Hoppers</a></li>
+   <li><a className="dropdown-item" href="https://en.disneyme.com/movies/the-devil-wears-prada-2">The Devil Prada2</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/star-wars-the-mandalorian-and-grogu">Star Wars:The Mandalorian and Grogu</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/toy-story-5">Toy Story 5</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/moana-2026">Moana</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/avengers-doomsday">Avengers:Doomday</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/lucasfilm">Lucasfilm Movies</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/marvel">Marvel Movies</a></li>
+    <li><a className="dropdown-item" href="https://en.disneyme.com/movies/pixar">Pixar Movies</a></li>
+  </ul>
+</div>
+  </div>
+</div>
+    </div>
+</div>
+ </section>
+    </section>
+    
+
+
+
+    
+    </>
+  )
+}
+
+export default Header
