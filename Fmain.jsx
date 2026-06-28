292239c3ea7baac4890fe1a65b07d73966dc3b87
mahanmg343-star <mahanmg343@gmail.com>
2026-06-28 22:14:03 +0330

    Add files via upload

---
+++ Fmain.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+
+function Fmain() {
+  return (
+
+<>
+
+<section className='container-fluid'>
+    <section className='row'>
+    <div className='d-none d-md-block'>
+
+<a className='' href="https://en.disneyme.com/movies/toy-story-5">
+
+    <img className='d-flex w-100 h-100' src="https://lumiere-a.akamaihd.net/v1/images/en_toystory5_hero_r_048a8876.jpeg?region=0,0,3200,1372&width=1200" alt="toystory5" />
+</a>
+   
+    </div>
+    </section>
+</section>
+
+<section className='container-fluid'>
+    <section className='row'>
+    <div className='d-md-none'>
+
+<a className='' href="https://en.disneyme.com/movies/toy-story-5">
+
+<img className='ifmm' src="	https://lumiere-a.akamaihd.net/v1/images/en_toystory5_hero_mob-v2_m_61d41c9c.jpeg?region=0,0,750,564" alt="" />
+</a>
+    </div>
+   
+    </section>
+</section>
+
+
+<a className='incna' href="https://en.disneyme.com/movies/toy-story-5">in cinemas now</a>
+
+
+
+<a className='d-flex justify-content-center incna' href="https://www.toystorymovie.me/?int_cmp=dol:xm:home:toy-5:homepage:0626:powster&campaign=homepageexternal">
+
+<button className=' incn'>BOOK TICKETS</button>
+</a>
+
+
+<a className='d-flex justify-content-center incna' href="https://www.toystorymovie.me/?int_cmp=dol:xm:home:toy-5:homepage:0626:powster&campaign=homepageexternal">
+
+<button className=' incn2'>FIND OUT MORE</button>
+</a>
+
+
+
+
+
+
+
+
+
+
+
+</>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  )
+}
+
+export default Fmain
