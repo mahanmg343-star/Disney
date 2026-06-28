f5440ed048f7e21d297ec76e70a839dc7afb575e
mahanmg343-star <mahanmg343@gmail.com>
2026-06-28 22:33:15 +0330

    Add files via upload

---
+++ Smain.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+
+function Smain() {
+  return (
+    <>
+    <section className='container'>
+        <section className='row mt-3 g-3'>
+            <div className='col-11 col-md-6'>
+                <div className="card width: 18rem;">
+                    <a href="http://localhost:3000/Disney">
+  <img src="https://lumiere-a.akamaihd.net/v1/images/eu_avatar_fire_and_ash_mobile_3042b115.jpeg?region=0,0,800,600&width=600" className="card-img-top" alt="avatar"/>
+                    </a>
+  <div className="card-body">
+    <h4>Avatar:Fire and Ash</h4>
+    <p className="card-text">Now streaming on Disney+.</p>
+    <h6>DISNEY+</h6>
+  </div>
+</div>
+            </div>
+
+
+            <div className=' col-11 col-md-6'>
+                <div className="card width: 18rem;">
+                    <a href="https://www.apps.disneyplus.com/movies/hoppers/1271432167?utm_source=Referral&utm_medium=Disney_WebsiteMENA&utm_campaign=Backlink&utm_content=Hoppers">
+  <img src="https://lumiere-a.akamaihd.net/v1/images/hoppers-disney-plus_mob_e8a2c8c6.jpeg?region=0,0,800,600&width=768" className="card-img-top" alt="hoppers"/>
+                    </a>
+  <div className="card-body">
+    <h4>Avatar:Fire and Ash</h4>
+    <p className="card-text">Now streaming on Disney+.</p>
+    <h6>DISNEY+</h6>
+  </div>
+</div>
+            </div>
+       
+
+
+        </section>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default Smain
