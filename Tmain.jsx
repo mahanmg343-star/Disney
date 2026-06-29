67c2402a54dd7b89632ec6ce8c7af26600efae03
mahanmg343-star <mahanmg343@gmail.com>
2026-06-29 15:11:07 +0330

    Add files via upload

---
+++ Tmain.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+
+function Tmain() {
+  return (
+    <>
+    <div className='dj'>
+    <h2 className='whsg'>What's on Disney+</h2>
+
+ <section className='container'>
+    <section className='row'>
+    <div className='d-flex'>
+
+<div className="w-50 m-3">
+    <a href="https://www.apps.disneyplus.com/shows/the-bear/1260108974?utm_source=Referral&utm_medium=Disney_WebsiteMENA&utm_campaign=Backlink&utm_content=TheBear">
+
+  <img src="https://lumiere-a.akamaihd.net/v1/images/en_the-bear-s5_tn_hulu_18_a3408871.jpeg?region=0,0,720,406&width=320" className="card-img-top w-100 " alt="thebear"/>
+    </a>
+  <div className="card-body">
+    <a className='dene' href="https://www.apps.disneyplus.com/shows/the-bear/1260108974?utm_source=Referral&utm_medium=Disney_WebsiteMENA&utm_campaign=Backlink&utm_content=TheBear">
+    <h5 className="catex ">The Bear</h5>
+    </a>
+   <p className='catex'>18+</p>
+  </div>
+</div>
+
+
+<div className="w-50 m-3">
+    <a className='dene' href="https://www.apps.disneyplus.com/shows/tucci-in-italy/1271406830?utm_source=Referral&utm_medium=Disney_WebsiteMENA&utm_campaign=Backlink&utm_content=TucciinItaly">
+  <img src="https://lumiere-a.akamaihd.net/v1/images/en_tucci-in-italy_s2_tn_e9749d74.jpeg?region=0,0,720,406&width=320" className="card-img-top w-100 " alt="thebear"/>
+
+    </a>
+  <div className="card-body">
+    <a className='dene' href="https://www.apps.disneyplus.com/shows/tucci-in-italy/1271406830?utm_source=Referral&utm_medium=Disney_WebsiteMENA&utm_campaign=Backlink&utm_content=TucciinItaly">
+        <h5 className="catex">Tucci in Italy</h5>
+        </a>
+   
+  </div>
+</div>
+
+
+
+<div className="w-50 m-3 d-none d-md-block">
+    <a href="https://www.apps.disneyplus.com/shows/rivals/1271326524?utm_source=Referral&utm_medium=Disney_WebsiteMENA&utm_campaign=Backlink&utm_content=Rivals">
+
+  <img src="https://lumiere-a.akamaihd.net/v1/images/en_rivals-s2_tn_hulu_18_c64225db.jpeg?region=0,0,720,406&width=320" className="card-img-top w-100 " alt="thebear"/>
+    </a>
+  <div className="card-body">
+    <a className='dene' href="https://www.apps.disneyplus.com/shows/rivals/1271326524?utm_source=Referral&utm_medium=Disney_WebsiteMENA&utm_campaign=Backlink&utm_content=Rivals">
+    <h5 className="catex ">Rivals</h5>
+    </a>
+   <p className='catex'>18+</p>
+  </div>
+</div>
+
+</div>
+
+
+<a className='d-flex justify-content-center incna' href="https://www.toystorymovie.me/?int_cmp=dol:xm:home:toy-5:homepage:0626:powster&campaign=homepageexternal">
+
+<button className=' incn27'>SIGN UP NOW*</button>
+</a>
+
+<p className='whsg2'>* Subscription required</p>
+
+
+
+
+
+
+
+    </section>
+
+ </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default Tmain
